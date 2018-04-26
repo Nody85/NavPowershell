44023eff1fa2ecd42b2a29db2796c707daf94ba2
Erik Anders <erikanders1@web.de>
2018-04-26 11:01:09 +0200

    Fix Assigning $NavIde after Importing Modules

--- Proof_PartnerRelease.ps1
+++ Proof_PartnerRelease.ps1
@@ -4,10 +4,10 @@
 #Step 3 Try to Re-Import the Objects if there is any Syntax Errror or you can not import an Object with your license, you will see it in the logpath to investigate
 
 $NavVersion = 100
-$NavIde="C:\Program Files (x86)\Microsoft Dynamics NAV\$NavVersion\RoleTailored Client\finsql.exe"
 Import-Module "C:\Program Files (x86)\Microsoft Dynamics NAV\$NavVersion\RoleTailored Client\NavModelTools.ps1"
 Import-Module "C:\Program Files (x86)\Microsoft Dynamics NAV\$NavVersion\RoleTailored Client\Microsoft.Dynamics.Nav.Ide.psm1"
 Import-module "C:\Program Files\Microsoft Dynamics NAV\$NavVersion\Service\NavAdminTool.ps1"
+$NavIde="C:\Program Files (x86)\Microsoft Dynamics NAV\$NavVersion\RoleTailored Client\finsql.exe"
 
 $DatabaseName = "MyDataBase"
 $WorkDir = "C:\Temp\Objects\"
