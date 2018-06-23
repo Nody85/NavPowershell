869ea1699819bb31c92d3ca64ccd320d13a46ece
Erik Anders <erikanders1@web.de>
2018-06-23 22:54:58 +0200

    Rename Files with Caption

---
+++ RenameFilesasBatch.ps1
@@ -0,0 +1,10 @@
+﻿#
+$OLDLANGUAGECODE = "DEU"
+$NEWLANGUAGECODE = "DES"
+
+$DefaultFiles =  Get-ChildItem | Where-Object {$_.Name -like "*$OLDLANGUAGECODE*"}
+ForEach($File in $DefaultFiles) 
+{
+    $newname = ([String]$File).Replace($OLDLANGUAGECODE,$NEWLANGUAGECODE)
+    Rename-item -Path $File $newname
+}
