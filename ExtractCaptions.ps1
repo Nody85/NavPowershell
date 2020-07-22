5b85fef5623f11b9b8c0f433989f8e7394e267c5
Erik Anders <ErikAnders1@web.de>
2020-07-22 17:26:54 +0200

    Steps documented more specifically

--- ExtractCaptions.ps1
+++ ExtractCaptions.ps1
@@ -18,7 +18,12 @@ $TargetVersion="NAV_CH_CU9"                               #NAV_CH_CU9
 $TargetVersion_File=$WorkDir+$TargetVersion+".txt"        #NAV_CH_CU9.txt
 $ProductVersion="PRODUCT_DE_CU9"                          #PRODUCT_DE_CU9 
 $ProductVersion_File=$WorkDir+$ProductVersion+".txt"      #PRODUCT_DE_CU9.txt
-$LanguageCodes="DEU","DES","ITS","FRS","ENU"
+
+$ProductCaptionLayer ="DEU"
+$DefaultCaptionLayer="DES","ITS","FRS","ENU"
+
+
+$DatabaseName="Compiling Database"
 
 #Step 1
 Write-Host "Step 1" -ForegroundColor Green
@@ -26,19 +31,20 @@ $TargetVersionCaptionPath=$WorkDir+$TargetVersion+"_Caption"
 MD $TargetVersionCaptionPath
 $ProductVersionCaptionPath=$WorkDir+$ProductVersion+"_Caption"
 MD $ProductVersionCaptionPath
-Export-NAVApplicationObjectLanguage -Destination $TargetVersionCaptionPath -Source $TargetVersion_File -LanguageId $LanguageCodes
-Export-NAVApplicationObjectLanguage -Destination $ProductVersionCaptionPath -Source $ProductVersion_File -LanguageId $LanguageCodes
+Export-NAVApplicationObjectLanguage -Destination $TargetVersionCaptionPath -Source $TargetVersion_File -LanguageId $DefaultCaptionLayer
+Export-NAVApplicationObjectLanguage -Destination $ProductVersionCaptionPath -Source $ProductVersion_File -LanguageId $ProductCaptionLayer
 
 #Step 2
 Write-Host "Step 2" -ForegroundColor Green
 $BaseVersionCaptionless_File=$WorkDir+$BaseVersion+"_Captionless.txt"
 $TargetVersionCaptionless_File=$WorkDir+$TargetVersion+"_Captionless.txt"
 $ProductVersionCaptionless_File=$WorkDir+$ProductVersion+"_Captionless.txt"
-Remove-NAVApplicationObjectLanguage -Destination $BaseVersionCaptionless_File -Source $BaseVersion_File -LanguageId $LanguageCodes
-Remove-NAVApplicationObjectLanguage -Destination $TargetVersionCaptionless_File -Source $TargetVersion_File -LanguageId $LanguageCodes
-Remove-NAVApplicationObjectLanguage -Destination $ProductVersionCaptionless_File -Source $ProductVersion_File -LanguageId $LanguageCodes
+Remove-NAVApplicationObjectLanguage -Destination $BaseVersionCaptionless_File -Source $BaseVersion_File -LanguageId $DefaultCaptionLayer
+Remove-NAVApplicationObjectLanguage -Destination $TargetVersionCaptionless_File -Source $TargetVersion_File -LanguageId $DefaultCaptionLayer
+Remove-NAVApplicationObjectLanguage -Destination $ProductVersionCaptionless_File -Source $ProductVersion_File -LanguageId $ProductCaptionLayer
 
 #Step 3
+#Warning Split-NAVApplicationObjectFile changes bad variable names
 Write-Host "Step 3" -ForegroundColor Green
 $BaseVersionCaptionless_Path=$WorkDir+$BaseVersion+"_Captionless"
 $TargetVersionCaptionless_Path=$WorkDir+$TargetVersion+"_Captionless"
@@ -47,36 +53,31 @@ Split-NAVApplicationObjectFile -Source $BaseVersionCaptionless_File -Destination
 Split-NAVApplicationObjectFile -Source $TargetVersionCaptionless_File -Destination $TargetVersionCaptionless_Path -Force
 Split-NAVApplicationObjectFile -Source $ProductVersionCaptionless_File -Destination $ProductVersionCaptionless_Path -Force
 
+#Step 4
 $ResultPath=$WorkDir+"MergeResult"
 MD $ResultPath
 Merge-NAVApplicationObject -ModifiedPath $TargetVersionCaptionless_Path -OriginalPath $BaseVersionCaptionless_Path -ResultPath $ResultPath -TargetPath $ProductVersionCaptionless_Path -DateTimeProperty FromTarget
 
+#Merge failed Objects with KDIFF3
+#Quality Gate 1 Compile without Captionlayer
+#Import all Objects and fix syntax errors
+#Compile all Objects
+$WorkDir = $ResultPath
+$WorkDirLogPath = $WorkDir+"LogPath"
+$ReImportEveryFile = $SplitPath+"*.txt"
+Import-NAVApplicationObject -DatabaseName $DatabaseName -Path $ReImportEveryFile -ImportAction Overwrite -LogPath $WorkDirLogPath -SynchronizeSchemaChanges Force -Confirm:$false
 
+#Step 5
+#Export all Objects to "SOURCECODEFOLDER"
+Import-NAVApplicationObjectLanguage -Destination "CAPTIONIMPORTRESULT1" -LanguagePath "LANGUAGEFOLDER" -Source "SOURCECODEFOLDER" -LanguageId $ProductCaptionLayer
+Import-NAVApplicationObjectLanguage -Destination "CAPTIONIMPORTRESULT2" -LanguagePath "LANGUAGEFOLDER" -Source "CAPTIONIMPORTRESULT1" -LanguageId $DefaultCaptionLayer
 
-#$LanguageCodes | %{
- 
-#  write-host "Adding Language" $PSItem -ForegroundColor Red
-#  if($LanguageCounter -eq 0)
-#  {
-#    Import-NAVApplicationObjectLanguage -Destination $Workdir$LanguageCounter -LanguagePath $ProductVersionCaptionPath\ -Source $ResultPath
-#  }
-#  else
-#  {
-#  }
-#  $LanguageCounter++
-
- 
-#}
-
-#Rest noch brauchbar ??
-
-#Import-NAVApplicationObjectLanguage -Destination "C:\GIT\dynamicsnav\Objects\" -LanguagePath ($WorkDir\CU4 DE CAPTION\") -Source "C:\Users\AndersE\Desktop\Install\CU4 DE CAPTIONless\" -LanguageId "DEU"
-#Export-NAVApplicationObjectLanguage -Destination "$WorkDir\CU4 SWISS CAPTION.txt" -Source "C:\GIT\dynamicsnav\Objects\" -LanguageId "DES","ITS","FRS","ENU"
-#Import-NAVApplicationObjectLanguage -Destination "$WorkDir\CU4 DE CAPTIONDE.txt" -LanguagePath "$WorkDir\CU4 DE ZU DES CAPTION.txt" -Source "$WorkDir\CU4 DE CAPTIONless.txt" -LanguageId DES
-#Import-NAVApplicationObjectLanguage -Destination "$WorkDir\CU4 DE CAPTIONDESITSFRS.txt" -LanguagePath "$WorkDir\CU4 SWISS CAPTION.txt" -Source "$WorkDir\CU4 DE CAPTIONDE.txt" -LanguageId "DES","ENU","FRS","ITS"
-#Export-NAVApplicationObjectLanguage -Destination "$WorkDir\CAPTION DE\" -Source "$WorkDir\Unitop2018_CU4.txt" -LanguageId "DEU"
-#Export-NAVApplicationObjectLanguage -Destination "$WorkDir\CAPTION DES" -Source "$WorkDir\GH_2018_CU4.txt" -LanguageId "DES","ITS","FRS","ENU"
-#Import-NAVApplicationObjectLanguage -Destination "$WorkDir\CU4 DE CAPTIONless_Schritt1.txt" -LanguagePath ("$WorkDir\CAPTION DEDES\") -Source ("$WorkDir\CU4 DE CAPTIONless.txt") -LanguageId "DES"
-#Import-NAVApplicationObjectLanguage -Destination "$WorkDir\CU4 DE CAPTIONless_Schritt2.txt" -LanguagePath ("$WorkDir\CAPTION DES\") -Source ("$WorkDir\CU4 DE CAPTIONless_Schritt1.txt") -LanguageId "DES","FRS","ITS","ENU"
-#Import-NAVApplicationObject -DatabaseName 2018_CH -Path C:\GIT\dynamicsnav\Objects\*.txt -ImportAction Overwrite -LogPath "$WorkDir\LOG\"
-#Import-NAVApplicationObject -DatabaseName 2018_CH -Path C:\GIT\SWISSMOD2018\*.txt -ImportAction Overwrite -LogPath "$WorkDir\LOG\"
+#Quality Gate 2 Compile with Captionlayer
+#Compile all Objects
+$WorkDir = $ResultPath
+$WorkDirLogPath = $WorkDir+"LogPath"
+$ReImportEveryFile = $SplitPath+"*.txt"
+Import-NAVApplicationObject -DatabaseName $DatabaseName -Path "CAPTIONIMPORTRESULT2" -ImportAction Overwrite -LogPath $WorkDirLogPath -SynchronizeSchemaChanges Force -Confirm:$false
+#Export
+#Split
+#Commit in Git
